cea8d0682d9ad68d14a09d32d0f2a3a9d001b979
GiuseppeDalessio7 <g.dalessio03@gmail.com>
2023-10-01 17:12:59 +0200

    Work to milestone 3

--- src/components/searchButton.vue
+++ src/components/searchButton.vue
@@ -48,10 +48,6 @@ export default {
 
 
         },
-
-        searchTV() {
-
-        },
         //faccio un metodo dove prendo il mio url e lo cambio in modo dinamico
         searchFilms() {
 
@@ -68,11 +64,16 @@ export default {
 
 
 <template>
-    <div class="container mt-4 ">
-
+    <div class="d-flex">
         <input v-model="store.films" class="p-1 mx-2" type="text" name="search" placeholder="Cerca Film o Serie" id=""
             required>
-        <button class="btn btn-outline-danger" @click="searchFilms()">cerca</button>
+        <button class="btn btn-outline-danger" @click="searchFilms()"> <span><i class="fa-solid fa-magnifying-glass fa-sm"
+                    style="color: #ffffff;"></i></span> <span class=" mx-1">Cerca</span></button>
+
+    </div>
+    <div class="container mt-4 ">
+
+
         <div class="container d-flex flex-wrap">
             <div class="row mt-2 ">
                 <div class=" col-lg-4 col-md-3 col-sm-4 mb-3 " v-for="(film, i) in this.store.filmScelto">
@@ -87,10 +88,12 @@ export default {
                         </h6>
                         <small>LINGUA:
                             <img id="sconosciuto" class="language_flag " :src="getFlagImage(film)" alt=""></small>
-                        <small>
-                            VOTO: {{ film.vote_average }}
-                        </small>
-
+                        <div>
+                            <small>
+                                VOTO: {{ film.vote_average }}<i class="fa-solid fa-star" style="color: #fff700;"></i>
+                                <i class="fa-regular fa-star" style="color: #fff700;"></i>
+                            </small>
+                        </div>
                     </div>
 
                 </div>
